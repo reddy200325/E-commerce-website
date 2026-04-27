c867016fbea69a46488d2e138b5cf6a0b5c1596a
Raghunadha Reddy <rreddy2512@gmail.com>
2026-04-27 10:49:44 +0530

    Delete Server directory

--- Server/config/MongoDB.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose"
-
-const connectDB =()=>{
- mongoose.connect(`${process.env.MONGODB_URI}/ecommerce`)
-}
-export default connectDB
--- Server/middleware/multer.js
+++
@@ -1,9 +0,0 @@
-import multer from 'multer';
-
-const storage = multer.diskStorage({
-    filename: (req, file, callback) => {
-        callback(null,file.originalname)
-    }
-})
-
-export const upload = multer({storage})
